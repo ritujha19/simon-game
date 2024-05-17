77a5b1e4242fcf98a63f03cb0975404c4caf9509
ritu jha <ritu05491@gmail.com>
2024-05-17 15:00:29 +0530

    add a start button

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ let buts= ["yellow","red","green","purple"];
 let started = false;
 let level = 0;
 let h2 = document.querySelector("h2");
-
-document.addEventListener("keypress", ()=>{
+let startBtn = document.querySelector("button");
+startBtn.addEventListener("click", ()=>{
     if(started == false){
         // console.log("game started");
         started = true;
